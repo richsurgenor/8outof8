74896c4d1f37bfb80944d37b4b928d3d078e4910
Rich Surgenor <krs0048@auburn.edu>
2018-08-12 15:56:31 -0400

    playing arund w/ impl of stack

--- chip8.c
+++ chip8.c
@@ -1,4 +1,6 @@
 #include "SDL2/SDL.h"
+#include "errno.h"
+#include "stdbool.h"
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
@@ -17,27 +19,58 @@ const int SCREEN_HEIGHT = 480;
 uint8_t ram[4096];
 
 // Relevant to CPU
+// Initialize
+
+#define REGISTER (*(volatile uint8_t*)0x10000)
 uint16_t pc;
 uint16_t stack[16];
+uint8_t sp;
 
 uint16_t Vx[16];
 uint16_t I;
 uint16_t delay_timer;
 uint16_t snd_timer;
 
-
-
-#define REGISTER (*(volatile uint8_t*)0x10000)
-
 //memory should be an 0xffff array? o-o
 
 int initSDL();
+void push(uint16_t instruction);
+bool pop(uint16_t *val);
 
-int main() {
+errno_t main() {
 	//initSDL();
+    pc = 0x200;
+    push(0);
+    push(1);
+    push(2);
+
+    uint16_t val;
+    while ((val = pop(&val))) {
+        return 0;
+    }
+
 	printf ("hello");
 	return 0;
 }	
+
+bool pop(uint16_t *val) {
+    return false;
+}
+
+void push(uint16_t instruction) { 
+    stack[sp] = instruction;
+    sp++;
+}
+
+errno_t execute () {
+    return 0;
+}
+
+int test() {
+    printf ("testing stack...");
+    pc = 0x0;
+    return 0;
+}
 
 int initSDL() {
 	//The window we'll be rendering to
